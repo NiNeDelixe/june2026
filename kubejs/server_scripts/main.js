abc09ece35e08cce1a41133341ff30578b777bc6
NiNeDelixe <alkor.sidius@gmail.com>
2026-06-03 02:59:03 +0300

    add some not changable mods

---
+++ kubejs/server_scripts/main.js
@@ -0,0 +1,11 @@
+// Visit the wiki for more info - https://kubejs.com/
+console.info('Hello, World! (Loaded server example script)')
+
+
+// ServerEvents.recipes(event => {
+//   event.replaceInput(
+//     { input: 'minecraft:stick' },
+//     'minecraft:stick',
+//     Ingredient.of('#minecraft:saplings')
+//   )
+// })
